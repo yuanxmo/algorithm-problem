494df534d40e9dd0a9ae6d0c135c991eb5b3ab34
yuanxmo <1764187345@qq.com>
2024-10-12 12:09:17 +0800

    code edit

--- leetcode/problem_1/main.c
+++ leetcode/problem_1/main.c
@@ -3,12 +3,8 @@ void quick_sort(int *nums, int l, int r) {
         return;
     int i = l - 1, j = r + 1, x = nums[l + r >> 1];
     while (i < j) {
-        do
-            i++;
-        while (nums[i] < x);
-        do
-            j--;
-        while (nums[j] > x);
+        do i++; while (nums[i] < x);
+        do j--; while (nums[j] > x);
         if (i < j) {
             int t = nums[i];
             nums[i] = nums[j];
